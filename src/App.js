28087ef78019e38907d982bd282b15938ed920d8
Ernestas Yushkiavichus <kenyrofa@gmail.com>
2024-07-27 14:01:01 +0300

    проблема перерисовак(необходимо использовать seMemo)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
 import MySelect from './components/UI/select/MySelect';
+import MyInput from './components/UI/input/MyInput';
 
 // nfn - снипет стрелочной функции
 // usf - useState
@@ -14,7 +15,24 @@ function App() {
     { id: 3, title: 'Javascript 3', body: 'Description' },
   ]);
 
-  const [selectedSort, setSelectedSort] = useState('');
+  const [selectedSort, setSelectedSort] = useState(''); // По чем сортируем Заголовок(title) или Описание(body)
+  const [searchQuery, setSearchQuery] = useState(''); // Состояние для поиска
+
+  function getSortedPost() {
+    console.log('ОТРАБОТАЛА ФУНКЦИЯ СОРТЕД ПОСТ')
+    if (selectedSort) {
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort])) // Механизм сортировки постов 
+    }
+    return posts
+
+  } // т.к selectedSort по умолчанию эта пустая строка наша проверка не отработает и выдаст ошибку
+  // т.к функция localCompare вызывается у несуществуещего поля и мы получаем undefind
+  // для этого создается эта функция
+
+
+
+  const sortedPost = getSortedPost()
+
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -26,7 +44,6 @@ function App() {
 
   const sortPosts = (sort) => {
     setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
   };
 
   return (
@@ -34,6 +51,11 @@ function App() {
       <PostForm create={createPost} />
       <hr style={{ margin: '15px 0' }} />
       <div>
+        <MyInput
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          placeholder="Поиск"
+        />
         <MySelect
           value={selectedSort}
           onChange={sortPosts}
@@ -45,7 +67,7 @@ function App() {
         />
       </div>
       {posts.length !== 0 ? (
-        <PostList remove={removePost} posts={posts} title="Посты про JS" />
+        <PostList remove={removePost} posts={sortedPost} title="Посты про JS" />
       ) : (
         <h1 style={{ textAlign: 'center', marginTop: '60px' }}>Посты не были найдены</h1>
       )}
