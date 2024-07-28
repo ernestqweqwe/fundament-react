957b2028e0d192fc1e5bf85308010bc4fe589dd1
Ernestas Yushkiavichus <kenyrofa@gmail.com>
2024-07-28 18:05:04 +0300

    кастомный хук useFetching

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePosts';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
+import { useFetcing } from './hooks/useFetching';
 
 // nfn - снипет стрелочной функции
 // usf - useState
@@ -17,7 +18,11 @@ function App() {
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
   const sortedAndSearchetPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostLoading, setIsPostLoading] = useState(false);
+
+  const [fetchPosts, isPostLoading, postError] = useFetcing(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -27,13 +32,6 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
-
-  async function fetchPosts() {
-    setIsPostLoading(true);
-    const posts = await PostService.getAll();
-    setPosts(posts);
-    setIsPostLoading(false);
-  }
 
   useEffect(() => {
     fetchPosts();
@@ -49,8 +47,11 @@ function App() {
       </MyModal>
       <hr style={{ margin: '15px 0' }} />
       <PostFilter filter={filter} setFilter={setFilter} />
+      {postError && <h1>Произошла ошибка {postError}</h1>}
       {isPostLoading ? (
-        <div style={{display:'flex', justifyContent:'center', marginTop:'180px'}}><Loader/></div>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '180px' }}>
+          <Loader />
+        </div>
       ) : (
         <PostList remove={removePost} posts={sortedAndSearchetPosts} title="Посты про JS" />
       )}
