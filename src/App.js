9a8e12809fe82c3a0475f633f423f9ee32921ab3
Ernestas Yushkiavichus <kenyrofa@gmail.com>
2024-07-28 10:36:00 +0300

    добавление анимации для добавоения и удаления постов

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ function App() {
   return (
     <div className="App">
       <MyButton style={{marginTop:30}} onClick={()=>setModal(true)}>
-        добавить статью
+        Добавить пост
       </MyButton>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import PostItem from './PostItem';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 function PostList({ posts, title, remove }) {
-  console.log(posts)
+  console.log(posts);
 
-  if(!posts.length) {
-    return (
-      <h1 style={{textAlign:'center', marginTop:'40px'}} >Посты не найдены</h1>
-    )
+  if (!posts.length) {
+    return <h1 style={{ textAlign: 'center', marginTop: '40px' }}>Посты не найдены</h1>;
   }
 
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>{title}</h1>
-      {posts.map((post, index) => (
-        <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
-      ))}
+      <TransitionGroup>
+        {posts.map((post, index) => (
+          <CSSTransition key={post.id} timeout={500} classNames="post">
+            <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </div>
   );
 }
