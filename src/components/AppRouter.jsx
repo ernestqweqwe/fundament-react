49b76caf22752cb8886db1b5f135cbbabc7027b3
Ernestas Yushkiavichus <kenyrofa@gmail.com>
2024-07-29 15:23:43 +0300

    приватнве и публичные маршруты

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import { routes } from '../router';
+import { publicRoutes, privateRoutes } from '../router';
 
 const AppRouter = () => {
-  return (
+  const isAuth = false;
+  return isAuth ? (
     <Routes>
-      {routes.map((route,i) => (
+      {privateRoutes.map((route, i) => (
         <Route key={i} Component={route.component} path={route.path} />
       ))}
       <Route path="/*" element={<Navigate to="/posts" replace />} />
+    </Routes>
+  ) : (
+    <Routes>
+      {publicRoutes.map((route, i) => (
+        <Route key={i} Component={route.component} path={route.path} />
+      ))}
+      <Route path="/*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,17 @@
 import About from '../pages/About';
 import PostIdPage from '../pages/PostIdPage';
 import Posts from '../pages/Posts';
+import Login from '../pages/Login';
 
-export const routes = [
+export const privateRoutes = [
   { path: '/about', component: About},
   { path: '/posts', component: Posts },
   { path: '/posts/:id', component: PostIdPage},
   
 ];
+
+export const publicRoutes = [
+    { path: '/login', component: Login},
+
+
+]
